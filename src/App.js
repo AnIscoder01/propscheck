75bdc22d63889354a5010181d6c1d5d383177d01
AnIscoder01 <99267983+AnIscoder01@users.noreply.github.com>
2022-03-15 11:32:35 +0100

    'statapp'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React from 'react';
-import Profile from './profile/Profile';
-import Pic from './profile/Pic';
+import React, { Component } from 'react'
+import ShowProfile from './components/ShowProfile'
 
-function App() {
-  const fullName="Anis";
-  const bio="Good Analytic Skills · Deep Logic Building et Problem Solving Skills · Great Understanding of Data Structures et Algorithms ·"
-  const profession="developper";
-  const pic="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300"
-  
-  
-  return (
-    <div className="App">
-
-       <Pic pic={pic}/>
-
-      <Profile fullName={fullName} bio={bio} profession={profession}/>     
-
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+        show : false
+    }
 }
-
-export default App;
+  render() {
+    return (
+      <div style={{display:'flex',justifyContent:'center'}}>  {this.state.show ? <ShowProfile/> : null}
+      <button onClick={()=>this.setState({show: !this.state.show})}>Show!
+      </button>
+      </div>
+    )
+  }
+}
--- src/profile/Pic.js
+++
@@ -1,7 +0,0 @@
-const Pic=({pic})=>{
-    return(
-        <img style={{'borderRadius':'50%',marginLeft:'500px'}} src={pic}/>
-        
-
-)}
-export default Pic;
